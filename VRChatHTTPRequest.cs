34b3eb3a10fcaa8e085264ba613d202b3cef4100
YukiYukiVirtual <yukiyukivirtual>
2020-10-19 22:23:23 +0900

    C#ソース作った。動くところまで完成

---
+++ VRChatHTTPRequest.cs
@@ -0,0 +1,276 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+class VRChatHTTPRequest
+{
+	const string SETTING_FILE_NAME = "setting.yaml";
+	static DateTime lastTime = DateTime.Now;
+	static Settings settings;
+	public static void Main()
+	{
+		// 多重起動抑止処理
+		Mutex mutex = new Mutex(false, "VRChatHTTPRequest");
+		if(!mutex.WaitOne(0, false))
+		{
+			System.Windows.Forms.MessageBox.Show("すでに起動しています。2つ同時には起動できません。", "多重起動禁止");
+			return;
+		}
+		
+		// 設定をインスタンス化
+		settings = new Settings(SETTING_FILE_NAME);
+		
+		// メイン処理
+		try{
+			WriteLog("Start");
+			
+			// CheckUpdateが有効なら更新をチェックさせる
+			if(settings.GetSettings() && settings.CheckUpdate)
+			{
+				WriteLog("CheckUpdateが有効です。");
+				OpenBrowser("https://github.com/YukiYukiVirtual/OpenBrowserServer/releases/");
+			}
+			ServerMain();
+		}
+		catch(Exception e)
+		{
+			WriteLog(e.ToString());
+			System.Windows.Forms.MessageBox.Show("予期しない例外が発生しました。logを参照してください。", "例外");
+			return;
+		}
+	}
+	static void ServerMain()
+	{
+		// サーバーを建てる
+		HttpListener listener = new HttpListener();
+		
+		// http://localhost/Temporary_Listen_Addresses/openURL/
+		listener.Prefixes.Add("http://+:80/Temporary_Listen_Addresses/openURL/");
+		listener.Start();
+		
+		while(true)
+		{
+			// リクエスト待ち
+			HttpListenerContext context = listener.GetContext();
+			// リクエストとレスポンス
+			HttpListenerRequest req = context.Request;
+			HttpListenerResponse res = context.Response;
+			
+			// QueryURLを取得する
+			string queryURL = req.RawUrl.Substring("/Temporary_Listen_Addresses/openURL/".Length);
+			
+				
+			// ブラウザを起動するかチェックする
+			// 起動する場合：起動するURLをログ出力する
+			// 起動しない場合：起動しない理由をログ出力する
+			bool canOpen = CheckURL(queryURL);
+			if(canOpen)
+			{
+				OpenBrowser(queryURL);
+			}
+			
+			// HTTPレスポンス
+			string outputString = "> " + queryURL + "\n" + canOpen;
+			byte[] content = Encoding.UTF8.GetBytes(outputString);
+			
+			res.OutputStream.Write(content, 0, content.Length);
+			
+			res.StatusCode = 200;
+			
+			res.Close();
+		}
+	}
+	// 指定されたURLを開けるかチェックする
+	static bool CheckURL(string queryURL)
+	{
+		Uri uri;
+		
+		// Uriオブジェクトを生成する
+		try{
+			uri = new Uri(queryURL);
+		}
+		catch(UriFormatException e)
+		{
+			WriteLog("指定されたURLが有効ではありません。[" + queryURL + "]");
+			if(e==null){}
+			return false;
+		}
+		
+		// 設定読み込み
+		if(!settings.GetSettings())
+		{
+			WriteLog("設定を読み込めませんでした。");
+			return false;
+		}
+		
+		// 呼び出し間隔チェック
+		TimeSpan ts = DateTime.Now - lastTime;
+		if(ts.TotalMilliseconds < settings.IdlePeriod)
+		{
+			WriteLog("呼び出し間隔が設定値未満です。[" + ts.TotalMilliseconds + "]");
+			return false;
+		}
+		
+		// プロトコルチェック
+		{
+			bool p = false;
+			foreach(string s in settings.Protocol)
+			{
+				if(uri.Scheme == s)
+				{
+					p = true;
+				}
+			}
+			if(!p)
+			{
+				WriteLog("許可されたプロトコルではありません。[" + queryURL　+ "]");
+				return false;
+			}
+		}
+		
+		// ドメインチェック
+		{
+			bool p = false;
+			foreach(string s in settings.Domain)
+			{
+				if(uri.Host == s
+					||
+				   uri.Host.EndsWith("." + s)
+				)
+				{
+					p = true;
+				}
+			}
+			if(!p)
+			{
+				WriteLog("許可されたドメインではありません。[" + queryURL + "]");
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	// 既定のブラウザでURLを開く
+	// 呼び出し間隔の基準時刻を更新する
+	static void OpenBrowser(string queryURL)
+	{
+		WriteLog("ブラウザを開きます。[" + queryURL + "]");
+		
+		ProcessStartInfo psi = new ProcessStartInfo("cmd.exe", "/k start \"\" \"" + queryURL + "\"");
+		psi.CreateNoWindow = true;
+		psi.UseShellExecute = false;
+		
+		Process.Start(psi);
+		
+		lastTime = DateTime.Now;
+	}
+	// ログを書く
+	static void WriteLog(string str)
+	{
+		Console.WriteLine(str);
+		using (var writer = new StreamWriter("VRChatHTTPRequest.log", true))
+		{
+			writer.WriteLine(str + " at " + DateTime.Now.ToString());
+		}
+	}
+}
+
+
+// 設定を読み込むクラス
+// パブリックメンバーをReadして使う
+class Settings{
+	public bool CheckUpdate;
+	public int IdlePeriod;
+	public List<string> Protocol;
+	public List<string> Domain;
+	
+	private string filename;
+	// インスタンス生成、設定の初期値を作る
+	public Settings(string filename)
+	{
+		this.filename = filename;
+		CheckUpdate = true;
+		IdlePeriod = 1000;
+		Protocol = new List<string>();
+		Domain = new List<string>();
+	}
+	// 設定ファイルを読み込む
+	public bool GetSettings()
+	{
+		try{
+			// テキストファイルをUTF-8で処理する
+			using(StreamReader sr = new StreamReader(filename, Encoding.GetEncoding("UTF-8") ) )
+			{
+				string data = sr.ReadToEnd();	// 全部取得する
+				// 設定名が始まる行を探す
+				int index_up = data.IndexOf("CheckUpdate:");
+				int index_ip = data.IndexOf("IdlePeriod:");
+				int index_p  = data.IndexOf("Protocol:");
+				int index_d  = data.IndexOf("Domain:");
+				
+				// 設定名を除去したもの
+				string str_up = data.Substring(index_up + "CheckUpdate:".Length);
+				string str_ip = data.Substring(index_ip + "IdlePeriod:".Length);
+				string str_p  = data.Substring(index_p  + "Protocol:".Length);
+				string str_d  = data.Substring(index_d  + "Domain:".Length);
+				
+				// CheckUpdateを取得する
+				{
+					StringReader srr = new StringReader(str_up);	// 1行目を取得する
+					CheckUpdate = srr.ReadLine().IndexOf("true") != -1;	// "true"が含まれているかをCheckUpdateに設定する
+				}
+				// IdlePeriodを取得する
+				{
+					int index_dec = str_ip.IndexOfAny(new char[]{'0','1','2','3','4','5','6','7','8','9'});	// 数字で始まるインデックス
+					str_ip = str_ip.Substring(index_dec);	// 数字で始まる文字列
+					StringReader srr = new StringReader(str_ip);	// 1行目を取得する
+					if(Int32.TryParse(srr.ReadLine(), out IdlePeriod))	// 1行目を取得して、数値に変換してIdlePeriodに入れようとする
+					{
+					}
+					if(IdlePeriod <= 0)	// 数値に変換できなければ、代わりに1000を入れる
+					{
+						IdlePeriod = 1000;
+					}
+				}
+				// ProtocolとDomainを取得する
+				Protocol = GetDataFromRaw(str_p);
+				Domain   = GetDataFromRaw(str_d);
+			}
+			return true;
+		}
+		catch(Exception e)
+		{
+			if(e == null){}
+			return false;
+		}
+	}
+	// 生のyamlから設定項目のリストを取得する
+	// yamlパースできるとは言っていない
+	// フォーマット↓
+	// 対象の設定名
+	//  - 設定項目
+	//  - 設定項目
+	// 対象以外の設定名
+	static List<string> GetDataFromRaw(string str)
+	{
+		List<string> list = new List<string>();	// いったんリストに入れてから最後に配列に変換する
+		string line;
+		StringReader sr = new StringReader(str);
+		sr.ReadLine();// 設定名なので読み飛ばす
+		while((line = sr.ReadLine()) != null)
+		{
+			int index_minus = line.IndexOf("-");	// "-"が含まれる行は設定項目ですという仕様
+			if(index_minus == -1)	// "-"が含まれなければ終わる
+			{
+				break;
+			}
+			line = line.Substring(index_minus+1).Trim();	// 設定項目を抜き出す
+			list.Add(line);
+		}
+		return list;
+	}
+}
